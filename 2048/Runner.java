0a931a6860c8b005de522bdbacf53c563f4a61c9
rsoong <rsoong@hcrhs.org>
2014-04-02 10:04:38 -0400

    Keystrokes Added

--- 2048/Runner.java
+++ 2048/Runner.java
@@ -10,5 +10,36 @@ public class Runner {
     public static void main(String[] args) {
         ActorWorld world = new ActorWorld(new BoundedGrid(4,4));
         world.show();
+        
+	 java.awt.KeyboardFocusManager.getCurrentKeyboardFocusManager() 
+	 .addKeyEventDispatcher(new java.awt.KeyEventDispatcher() { 
+	 public boolean dispatchKeyEvent(java.awt.event.KeyEvent event) { 
+	 String key = javax.swing.KeyStroke.getKeyStrokeForEvent(event).toString(); 
+	 if (key.equals("pressed UP")) 
+	 {
+	 	2048.move(0);
+	 }
+	
+	 if (key.equals("pressed RIGHT"))
+	 {
+	 	2048.move(90);
+	 } 
+	 
+	 if (key.equals("pressed DOWN")) 
+	 {
+	 	2048.move(180);
+	 }
+	
+	 if (key.equals("pressed LEFT")) 
+	 {
+	 	2048.move(270);
+	 }
+	
+	 return true; 
+	 } 
+	 	
+	 }); 
+        
+        
     }
 }
