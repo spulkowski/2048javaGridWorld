6ccb91d69682b312aaca3fe77d804e87f8151bbc
Petros Ring <me@petros789.com>
2014-04-04 09:20:43 -0400

    HULK Smash Complete, Requires Moving Man

--- 2048/Runner.java
+++ 2048/Runner.java
@@ -7,6 +7,7 @@ import info.gridworld.grid.*;
 import info.gridworld.actor.*;
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Runner {
     public static void main(String[] args) {
@@ -60,21 +61,20 @@ class Logic extends Actor {
 	}
 
 	public void move(int direction) {
-		// BIGGEST ISSUE: ALL THE NULL POINTER ERRORS EVAR (Apparently Colors Be Stupid Dawg)
 		// Transveral Still Not Implemented in this Situation
 		ArrayList<Location> actors = getGrid().getOccupiedLocations();
 		for(Location loc : actors) {
 			if (getGrid().get(loc.getAdjacentLocation(direction)) instanceof Rock && getGrid().isValid(loc.getAdjacentLocation(direction))) {
-				// The Below Line Doesn't Work and Is Driving Me Mad But Is Basically Supposed to Just Check if the Two Rocks are the Same Color
 				if(getGrid().get(loc.getAdjacentLocation(direction)).getColor().equals(getGrid().get(loc).getColor())) {
-					// Update Next Ones Color and Delete the Original (Tested and Doesn't Work...YAY .___.)
-					getGrid().got(loc).removeSelfFromGrid();
-					//getGrid().get(loc.getAdjacentLocation(direction)).setColor(nextColor(getGrid().get(loc.getAdjacentLocation(direction)).getColor()));
-				}
-				//System.out.println(getGrid().get(loc).getColor());
-				//System.out.println(getGrid().get(loc.getAdjacentLocation(direction)).getColor());
-				//System.out.println("=====================");
+					getGrid().get(loc).removeSelfFromGrid();
+					getGrid().get(loc.getAdjacentLocation(direction)).setColor(nextColor(getGrid().get(loc.getAdjacentLocation(direction)).getColor()));
+				} 
 			}
+			//if(getGrid().get(loc.getAdjacentLocation(direction)) == null && getGrid().isValid(loc.getAdjacentLocation(direction))) {
+				
+			//	getGrid().put(loc.getAdjacentLocation(direction), getGrid().get(loc));
+				
+			//}
 		}
 	}
 
@@ -87,7 +87,7 @@ class Logic extends Actor {
 		}
 		return Color.yellow;
 	}
-	/*
+	
 	public void randomTile()
 	{
 		boolean status = false;
@@ -95,17 +95,16 @@ class Logic extends Actor {
 		Random rand = new Random();
 		int x = 0;
 		int y = 0;
-		Location loc = new Location;
 		while(status == false) 
 			x = rand.nextInt(4);
 			y = rand.nextInt(4);
-			loc = (x,y);
+			Location loc = new Location(x,y);
 			if(gr.get(loc) == null)
 			{
 				
 			}
 	}
-	*/
+
 }
 
 class Score {
